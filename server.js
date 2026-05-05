87be1c1843adb6f13d98257fb2aa0b781d0b20fe
MAMINAGORDOST15 <slavikgo0915@gmail.com>
2026-05-06 01:54:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 const PORT = process.env.PORT || 3000;
