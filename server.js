e117025e952e9a8c155cdd385c738d8f5f57934b
MAMINAGORDOST15 <slavikgo0915@gmail.com>
2026-05-06 01:57:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res) => {
+app.get('*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
